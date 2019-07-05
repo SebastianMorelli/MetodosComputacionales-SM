3545131bc7aedc579e03f4faee34c45a18430a1a
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-04 21:26:05 -0500

    Create factorial.cpp

---
+++ Clases/Semana 4/Clase 4 Tarea/factorial.cpp
@@ -0,0 +1,31 @@
+//Este código halla el factorial de un número 
+
+#include <iostream>
+using namespace std;
+
+//Declaración Variables 
+
+int n1 = 7;
+int n2 = 77;
+
+//Declaración funciones
+
+int fact(int);
+
+int main()
+{
+  cout<<fact(n1)<<endl;
+  cout<<fact(n2)<<endl;
+  
+  return 0;
+}
+
+int fact (int Val)
+{
+  int cont = 1;
+  for(int i = 1; i <= Val; i ++)
+  {
+    cont = cont*i;
+  }
+  return cont;
+}
