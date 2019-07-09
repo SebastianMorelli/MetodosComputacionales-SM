bd0134a523d3fe6e03e12be3d88256f740bf3978
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-09 11:18:47 -0500

    Update MorelliSebastian_cuerda.cpp

--- Clases/Semana 6/Clase 2/MorelliSebastian_cuerda.cpp
+++ Clases/Semana 6/Clase 2/MorelliSebastian_cuerda.cpp
@@ -6,16 +6,17 @@ using namespace std;
 
 //Declaración Variables 
 
-int ini = 0;
-int fini = 2;
-int numpunt = 80;
-float vel = 1.0;
-float dx = (fini - ini)/numpunt;
-float dt = (dx/vel)*0.25;
+float long = 1.0;
+float velc = 300.0;
+float dx = 0.005;
+float dt = (dx/velc)*0.25;
+float numpunt = long/dx;
+float itera = 0.1/dt;
 
 //Declaración funciones
 
-float * yoAdivinoFuturo (float arrPast, float arrFut);
+float * yoAdivinoFuturos (float arrPast, float arrFut);
+float * yoReemplazoPasados (float arrPasd, float arrFutu);
 
 int main()
 {
@@ -32,20 +33,41 @@ int main()
         else{
             inicialWave[i]=2.0;
         }
+        cout<<inicialWave[i]<<" ";
     }
     
-    
-    
+    for (int i = 0; i < itera/100; i++){
+        for(int j = 0; j <= numpunt; j++){
+        
+            yoAdivinoFuturos (inicialWave, futureWave);
+            yoReemplazoPasados (inicialWave, futureWave);
+        
+            cout<<inicialWave[j]<<" ";
+        }
+    }   
 }
 
 
 float * yoAdivinoFuturo (float arrPast, flaot arrFut){
     
-    float * p = arrFut;
+    float * p0 = arrFut;
     
-    for( int i = 1; i <= numpunt; i++){
-    arrFut[i] = (vel*dt)/dx * (arrPast[i]-arrPast[i-1]) + arrPast[i];
+    for( int i = 1; i < numpunt; i++){
+        
+        arrFut[i] = (vel*dt)/dx * (arrPast[i]-arrPast[i-1]) + arrPast[i];
     }
     
-    return p;
+    return p0;
+}
+
+float * yoReemplazoPasados (float arrPasd, float arrFutu){
+
+    float * p1 = arrPast;
+    
+    for( int i = 1; i < numpunt; i++){
+        
+        arrPast[i] = arrFutu[i];
+    }
+    
+    return p1;   
 }
