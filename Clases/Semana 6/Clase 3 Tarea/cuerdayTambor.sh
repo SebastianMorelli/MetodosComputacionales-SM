aa7407b137253de416dbf84c4b20c36b16ac6cd3
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-11 09:29:15 -0500

    Update cuerdayTambor.sh

--- Clases/Semana 6/Clase 3 Tarea/cuerdayTambor.sh
+++ Clases/Semana 6/Clase 3 Tarea/cuerdayTambor.sh
@@ -1,3 +1,16 @@
-g++ MorelliSebastian_cuerdayTambor.cpp
-./a.out > data.txt
-python3 Plots_cuerdayTambor.py
+#Este MakeFile corre los archivos de las cuerdas cons sus 3 condiciones.
+
+plotCuerdas.pdf: dataFijos.txt dataLibres.txt dataForzado.txt Plots_cuerdayTambor.py
+	python3 Plots_cuerdayTambor.py
+
+dataFijos.txt: MorelliSebastian_cuerdaFijos.cpp
+	g++ MorelliSebastian_cuerdaFijos.cpp
+	./a.out > dataFijos.txt
+
+dataLibres.txt: MorelliSebastian_cuerdaLibre.cpp 
+	g++ MorelliSebastian_cuerdaLibre.cpp
+	./a.out > dataLibres.txt
+
+dataForzado.txt: MorelliSebastian_cuerdaForzada.cpp
+	g++ MorelliSebastian_cuerdaForzada.cpp
+	./a.out > dataForzado.txt
