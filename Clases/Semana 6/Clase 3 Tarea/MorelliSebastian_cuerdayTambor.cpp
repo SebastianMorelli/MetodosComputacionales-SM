23ddfee3e34c7cd75d231dba34bcce3d2f6afdd9
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-11 08:09:19 -0500

    Update MorelliSebastian_cuerdayTambor.cpp

--- Clases/Semana 6/Clase 3 Tarea/MorelliSebastian_cuerdayTambor.cpp
+++ Clases/Semana 6/Clase 3 Tarea/MorelliSebastian_cuerdayTambor.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 float longi = 1.0;
 float velc = 300.0;
-float dx = 0.05;
+float dx = 0.005;
 float dt = (dx/velc)*0.25;
 float A0 = 0.1;
 int numpunt = longi/dx;
@@ -21,14 +21,26 @@ float * yoReemplazoPasados (float arrPasd[], float arrPresnt[],float arrFutu[]);
 
 int main()
 {
+    //Declaración Arrays
     float x[numpunt];
     float pastWave[numpunt];
     float presentWave[numpunt];
     float futureWave[numpunt];
- 
+    
+    //Condiciones de Frontera 
+    x[0] = 0;
+    x[numpunt] = 0;
+    pastWave[0] = 0;
+    pastWave[numpunt] = 0;
+    presentWave[0] = 0;
+    presentWave[numpunt] = 0;
+    futureWave[0] = 0;
+    futureWave[numpunt] = 0;
+    
+    //Condición inicial
     float j = 0;
     float k = A0;
-    for (int i = 0; i <= numpunt; i++){
+    for (int i = 1; i < numpunt; i++){
         float xRope = dx*i;
         if(xRope <= longi/2){
             j+=2*A0/numpunt;
@@ -39,53 +51,55 @@ int main()
             pastWave[i] = k;
         }
         x[i] = xRope;
-        cout<<x[i]<<" "<<pastWave[i]<<" "<<endl;    
+        cout<<x[i]<<" "<<pastWave[i]<<endl;    
     }
-        
+    
+    //Segunda condición inicial pasado dt
     for(int i = 1; i < numpunt; i++){
     
-        presentWave[i] = ((pow(velc,2)*pow(dt,2))/2*pow(dx,2)) * (pastWave[i+1] + pastWave[i-1] - 2*pastWave[i]) + pastWave[i];
-        cout<<x[i]<<" "<<presentWave[i]<<" "<<endl;
+        presentWave[i] = (((pow(velc,2)*pow(dt,2))/(2*pow(dx,2))) * (pastWave[i+1] + pastWave[i-1] - 2*pastWave[i])) + pastWave[i];
+        cout<<x[i]<<" "<<presentWave[i]<<endl;
     }
     
-//    for (int i = 0; i < itera; i++){
-//        
-//        int contador = 0;
-//        
-//        for(int j = 0; j <= numpunt; j++){
-//        
-//            yoAdivinoFuturos (pastWave, presentWave, futureWave);
-//            yoReemplazoPasados (pastWave, presentWave, futureWave);
-//        }
-//        
-//        contador += 1;
-//        if(contador == 20){
-//            cout<<presentWave[j]<<endl;
-//        }
-//    }    
+    //Output de las oiscilaciones futuras halladas
+    int contador = 0;
+    for (int i = 0; i < itera; i++){
+        
+        yoAdivinoFuturos (pastWave, presentWave, futureWave);
+        yoReemplazoPasados (pastWave, presentWave, futureWave);
+        
+        if(contador == 800){
+            for(int j = 0; j <= numpunt; j++){
+                cout<<x[j]<<" "<<presentWave[j]<<endl;
+                contador = 0;
+            }
+        }
+        
+        contador ++;
+    }    
 }
 
-//float * yoAdivinoFuturos (float arrPast[], float arrPres[], float arrFut[]){
-//    
-//    float * p0 = arrFut;
-//    
-//    for( int i = 1; i < numpunt; i++){
-//        
-//        arrFut[i] = (pow(velc,2)*pow(dt,2)/pow(dx,2)) * (arrPres[i]+arrPres[i]-2*arrPres[i-1]) -arrPast[i] + 2*arrPres[i];
-//    }
-//    
-//    return p0;
-//}
-//
-//float * yoReemplazoPasados (float arrPasd[], float arrPresnt[],float arrFutu[]){
-//
-//    float * p1 = arrPasd;
-//    
-//    for( int i = 1; i < numpunt; i++){
-//        
-//        arrPasd[i] = arrPresnt[i];
-//        arrPres[i] = arrFutu[i];
-//    }
-//    
-//    return p1;   
-//}
+float * yoAdivinoFuturos (float arrPast[], float arrPres[], float arrFut[]){
+    
+    float * p0 = arrFut;
+    
+    for( int i = 1; i < numpunt; i++){
+        
+        arrFut[i] = ((pow(velc,2)*pow(dt,2)/pow(dx,2)) * (arrPres[i+1]+arrPres[i-1]-2*arrPres[i])) -arrPast[i] + (2*arrPres[i]);
+    }
+    
+    return p0;
+}
+
+float * yoReemplazoPasados (float arrPasd[], float arrPresnt[],float arrFutu[]){
+
+    float * p1 = arrPresnt;
+    
+    for( int i = 1; i < numpunt; i++){
+        
+        arrPasd[i] = arrPresnt[i];
+        arrPresnt[i] = arrFutu[i];
+    }
+    
+    return p1;   
+}
