9439b055690ababbf11808bc2f4f464ec882c41f
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-12 11:42:54 -0500

    Update Difusion.cpp

--- Clases/Semana 6/Clase 5/Difusion.cpp
+++ Clases/Semana 6/Clase 5/Difusion.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cmath>
+#include <fstream>
 using namespace std;
 
 //Declaración Variables 
@@ -9,7 +10,7 @@ using namespace std;
 float lado = 1.0;
 float cofdif = 1e-4;
 float dx = 0.01;
-float dt = 1.0;
+float dt = 0.25*pow(dx,2)/cofdif;
 float ladopeq = 0.20;
 int numpunt = lado/dx;
 float itera = 2500.0;
@@ -18,85 +19,108 @@ float Tpeq = 100.0;
 
 //Declaración funciones
 
-float * yoAdivinoFuturos (float arrPast[], float arrPres[], float arrFut[]);
-float * yoReemplazoPasados (float arrPasd[], float arrPresnt[],float arrFutu[]);
+float * yoAdivinoFuturos (float arrPres[], float arrFut[]);
+float * yoReemplazoPasados (float arrPresnt[], float arrFutu[]);
 
 int main()
 {
+    ofstream outfile;
+    outfile.open("data.txt");
+    
     //Declaración Arrays
     float x[numpunt][numpunt];
-    float pastTxTy[numpunt][numpunt];
-    float presentTxTy[numpunt];
-    float futureTxTy[numpunt];
+    float presentTxTy[numpunt][numpunt];
+    float futureTxTy[numpunt][numpunt];
     
     //Condiciones de Frontera 
    
     
     //Condición inicial
 
-    for (int i = 0; i <= numpunt; i++){
+    for (int i = 0; i < numpunt; i++){
         float xlam = dx*i;
-        for(int j = 0; j<= numpunt; j++){
+        for(int j = 0; j < numpunt; j++){
             
             float ylam = dx*j;
             
             if(xlam >= 0.20 && xlam <=0.40 && ylam >= 0.40 && ylam <= 0.60){
-              pastTxTy[i][j] = Tpeq;
+              presentTxTy[i][j] = Tpeq;
             }
             else{
-              pastTxTy[i][j] = T0;
+              presentTxTy[i][j] = T0;
             }    
-            x[i][j] = xlam;
-            cout<<x[i][i]<<" "<<pastTxTy[i][j]<<" ";
+            
+            outfile<<presentTxTy[i][j]<<" ";
         }
+        outfile<<endl;
     }
     
-//    //Segunda condición inicial pasado un dt
-//    for(int i = 1; i < numpunt; i++){
-//    
-//        presentWave[i] = (((pow(velc,2)*pow(dt,2))/(2*pow(dx,2))) * (pastWave[i+1] + pastWave[i-1] - 2*pastWave[i])) + pastWave[i];
-//        cout<<x[i]<<" "<<presentWave[i]<<endl;
-//    }
-//    
-//    //Output de las oscilaciones futuras halladas
-//    int contador = 0;
-//    for (int i = 0; i < itera; i++){
-//        
-//        yoAdivinoFuturos (pastWave, presentWave, futureWave);
-//        yoReemplazoPasados (pastWave, presentWave, futureWave);
-//        
-//        if(contador == 500){
-//            for(int j = 0; j <= numpunt; j++){
-//                cout<<x[j]<<" "<<presentWave[j]<<endl;
-//                contador = 0;
-//            }
-//        }
-//        
-//        contador ++;
-//    }    
+    outfile.close();
+    
+    outfile.open("data100.txt");
+
+   //Output de las oscilaciones futuras halladas
+   int contador = 0;
+   for (int i = 0; i < itera; i++){
+       
+       yoAdivinoFuturos (presentTxTy, futureTxTy);
+       yoReemplazoPasados(presentTxTy, futureTxTy);
+       
+       for(int k=0; k <= numpunt; k++){
+           if(contador == 100){
+               for(int j = 0; j <= numpunt; j++){
+                   outfile<<presentTxTy[k][j]<<" ";
+               }
+           }
+       }
+       outfile<<endl;
+       contador ++;
+   }
+    
+   outfile.close();
+    
+   outfile.open("data2500.txt");
+    
+   int contador1 = 0;
+   for (int i = 0; i < itera; i++){
+       
+       yoAdivinoFuturos (presentTxTy, futureTxTy);
+       yoReemplazoPasados (presentTxTy, futureTxTy);
+       
+       for(int k=0; k <= numpunt; k++){
+           if(contador1 == 2500){
+               for(int j = 0; j <= numpunt; j++){
+                   outfile<<presentTxTy[k][j]<<" ";
+               }
+           }
+       }
+       outfile<<endl;
+       contador1 ++;
+   }
+   outfile.close();
 }
 
-//float * yoAdivinoFuturos (float arrPast[], float arrPres[], float arrFut[]){
-//    
-//    float * p0 = arrFut;
-//    
-//    for( int i = 1; i < numpunt; i++){
-//        
-//        arrFut[i] = ((pow(velc,2)*pow(dt,2)/pow(dx,2)) * (arrPres[i+1]+arrPres[i-1]-2*arrPres[i])) -arrPast[i] + (2*arrPres[i]);
-//    }
-//    
-//    return p0;
-//}
-//
-//float * yoReemplazoPasados (float arrPasd[], float arrPresnt[],float arrFutu[]){
-//
-//    float * p1 = arrPresnt;
-//    
-//    for( int i = 1; i < numpunt; i++){
-//        
-//        arrPasd[i] = arrPresnt[i];
-//        arrPresnt[i] = arrFutu[i];
-//    }
-//    
-//    return p1;   
-//}
+float * yoAdivinoFuturos (float arrPres[], float arrFut[]){
+    
+    float * p0 = arrFut;
+    
+    for( int i = 1; i < numpunt; i++){
+        for( int j = 1; j < numpunt; j++){
+            arrFut[i][j] = ((pow(cofdif,2)*dt,2/pow(dx,2)) * (2*arrPres[i+1]+2*arrPres[i-1]-4*arrPres[i])) + (arrPres[i]);
+        }
+    }
+    return p0;
+}
+
+
+float * yoReemplazoPasados (float arrPresnt[],float arrFutu[]){
+
+    float * p1 = arrPresnt;
+    
+    for( int i = 1; i < numpunt; i++){
+        for( int j = 1; j < numpunt; j++){
+            arrPresnt[i][j] = arrFutu[i][j];
+        }
+    }
+    return p1;   
+}
