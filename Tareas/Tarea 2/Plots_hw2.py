26c0161883e8bc74dd07bfc94dea3d9dd66d9a15
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-19 09:51:27 -0500

    Update Plots_hw2.py

--- Tareas/Tarea 2/Plots_hw2.py
+++ Tareas/Tarea 2/Plots_hw2.py
@@ -43,6 +43,8 @@ ax2.set_ylabel('$Posición$ $y$ [UA]', size = 15)
 ax2.set_title('Posición: dt = 0.001 - 20 Orbitas')
 ax2.legend()
 
+plt.savefig('XY_met_dt.pdf')
+
 #Plots Velocidad
 fig1 = plt.figure(figsize=(20,4))
 ax0 = fig1.add_subplot(131)
@@ -65,6 +67,8 @@ ax2.set_xlabel('$Velocidad$ $x$ [UA/yrs$^2$]', size = 15)
 ax2.set_ylabel('$Velocidad$ $y$ [UA/yrs$^2$]', size = 15)
 ax2.set_title('Velocidad: dt = 0.001 - 20 Orbitas')
 ax2.legend()
+
+plt.savefig('VxVy_met_dt.pdf')
 
 #Formula Momentum
 def MomentumAng (Posx, Posy, Velx, Vely, dt):
@@ -110,3 +114,46 @@ ax2.set_xlabel('$tiempo$  [yrs]', size = 15)
 ax2.set_ylabel('$Momentum Angular$ [Msol.UA$^2$/yrs]', size = 15)
 ax2.set_title('Momentum Angular: dt = 0.001 - 20 Orbitas')
 ax2.legend()
+
+plt.savefig('Mome_met_dt.pdf')
+
+#Formula Energía
+
+def Energon((Posx, Posy, Velx, Vely):
+    
+    massTi = 3.00273e-6
+    cineticX = 1/2 * massTi * Velx**2
+    cineticY = 1/2 * massTi * Vely**2
+    PotencialX = massTi*Posx
+    PotencialY = massTi*Posy
+            
+    Cinetic = np.sqrt(cineticX**2 + CineticY**2)
+    Potencial = np.sqrt(PotencialX**2 + PotencialY**2)
+    return Cinetic + Potencial
+            
+Energia_RG_dt0 = Energon(RG_x_dt0, RG_y_dt0, RG_vx_dt0, RG_vy_dt0)
+Energia_RG_dt1 = Energon(RG_x_dt1, RG_y_dt1, RG_vx_dt1, RG_vy_dt1)
+Energia_RG_dt2 = Energon(RG_x_dt2, RG_y_dt2, RG_vx_dt2, RG_vy_dt2)
+            
+fig3 = plt.figure(figsize=(28,4))
+ax0 = fig3.add_subplot(131)
+ax0.plot(np.log(RG_dt0), Energia_RG_dt0, 'black', label = ('RugenKutta'))
+ax0.set_xlabel('$tiempo$  [yrs]', size = 15)
+ax0.set_ylabel('$Energia$ [Msol.UA$^2$/yrs]', size = 15)
+ax0.set_title('Energia: dt = 0.1 - 2000 Orbitas')
+ax0.legend()
+
+ax1 = fig3.add_subplot(132)
+ax1.plot(np.log(RG_dt1), Energia_RG_dt1, 'black', label = ('RugenKutta'))
+ax1.set_xlabel('$tiempo$  [yrs]', size = 15)
+ax1.set_ylabel('$Energia$ [Msol.UA$^2$/yrs]', size = 15)
+ax1.set_title('Energia: dt = 0.01 - 200 Orbitas')
+ax1.legend()
+
+ax2 = fig3.add_subplot(133)
+ax2.plot(np.log(RG_dt2), Energia_RG_dt2, 'black', label = ('RugenKutta'))
+ax2.set_xlabel('$tiempo$  [yrs]', size = 15)
+ax2.set_ylabel('$Energia$', size = 15)
+ax2.set_title('Energia: dt = 0.001 - 20 Orbitas')
+            
+plt.savefig('Ener_met_dt.pdf')
