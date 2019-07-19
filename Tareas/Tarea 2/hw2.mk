eafec3dd383641b17381e2b7421c28801438ab25
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-19 09:50:46 -0500

    Update hw2.mk

--- Tareas/Tarea 2/hw2.mk
+++ Tareas/Tarea 2/hw2.mk
@@ -1,1 +1,11 @@
-que visaje la bida
+#Este MakeFile corre los archivos de la Segunda Tarea.
+
+FFtIm.pdf ImProceso.pdf ImHybrid.pdf XY_met_dt.pdf VxVy_met_dt.pdf Mome_met_dt.pdf Ener_met_dt.pdf: Fourier.py dataRG.txt Plots_hw2.py
+	python3 Fourier.py
+	python3 Plots_hw2.py
+
+dataRG.txt: a.out
+	./a.out > dataRG.txt
+
+a.out: ODEs.cpp
+	 g++ ODEs.cpp
